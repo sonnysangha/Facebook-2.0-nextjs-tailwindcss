eab513cf9d6444b38c22b721ede99d2d49887bb7
LawryFa <92519943+LawryFa@users.noreply.github.com>
2022-07-07 07:02:53 +0200

    First real commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-module.exports = {
-  images: {
-    domains: [
-      "links.papareact.com",
-      "platform-lookaside.fbsbx.com",
-      "firebasestorage.googleapis.com",
-    ],
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
